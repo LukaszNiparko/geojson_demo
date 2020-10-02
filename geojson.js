6c74c7c30c8655725058db00603f857d8e881be2
LukaszNiparko <71901010+LukaszNiparko@users.noreply.github.com>
2020-10-01 23:15:48 -0500

    One more update...

--- geojson.js
+++ geojson.js
@@ -1,15 +1,4 @@
-// 1. erase everything below this line and paste in your geojson
-// 2. add the following before your pasted content, so that the map
-//    will understand how to find your geojson. See README.md for more info
-//
-//    var json =
-//
-//    See example below:
-
 var json = {
-  "type": "FeatureCollection",
-  "features": [
-   {
   "type": "FeatureCollection",
   "features": [
     {
